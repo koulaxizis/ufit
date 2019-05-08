171bdecb1d5fa537b2ee4167611020e41ef93a8d
Christos Koulaxizis <koulaxizis@posteo.net>
2019-05-09 00:35:51 +0300

    Update uFit.sh

--- uFit.sh
+++ uFit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove && sudo apt-get autoremove --purge && sudo apt-get autoclean && sudo apt-get clean && sudo flatpak uninstall --unused && sudo snap set system refresh.retain=2 && xmessage "All done! Enjoy your Linux system! :D"
+sudo apt-get update && sudo apt-get install trash-cli && trash-empty 30 && sudo apt-get upgrade && sudo apt-get autoremove && sudo apt-get autoremove --purge && sudo apt-get autoclean && sudo apt-get clean && sudo flatpak uninstall --unused && sudo snap set system refresh.retain=2 && xmessage "All done! Enjoy your Linux system! :D"
