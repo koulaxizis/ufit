17c76c59102e2dbf72c5e57015ff26fb723e0f9a
Christos Koulaxizis <koulaxizis@pmail.gr>
2020-12-04 20:06:38 +0200

    Beautify

--- uFit.sh
+++ uFit.sh
@@ -16,11 +16,11 @@ echo "What it does...
  - Removes all packages kept in the apt cache.
  - Removes all data kept in the thumbnail cache.
  - Removes all unused flatpak runtimes.
- - Removes old versions on installed snaps.
+ - Removes old versions on installed snaps [*].
  - Removes trashed files older than 30 days.
  - Removes extra localization of installled packages."
 echo ""
-echo "If snapd isn't installed, the script will end unexpectedly. However, commands about Snap are at the very end, so all of the rest commands will have been executed successfully. Not sure (yet) how to fix that. Feel free to help if you know how. Alternatively, edit the script with your favorite text editor and remove '&& sudo snap refresh && sudo snap set system refresh.retain=2', save and run it!"
+echo "[*] If snapd isn't installed, the script will end unexpectedly. However, commands about Snap are at the very end, so all of the rest commands will have been executed successfully. Not sure (yet) how to fix that. Feel free to help if you know how. Alternatively, edit the script with your favorite text editor and remove 'sudo snap refresh' & 'sudo snap set system refresh.retain=2', save and run it!"
 echo ""
 echo "$(tput setaf 1)$(tput setab 7)Warning:Absolutely no warranty provided. I'm not responsible in any way for any possible damage may occur on your system. Use it at your own responsibility. Do you want to continue? $(tput sgr 0)"
 echo ""
@@ -31,4 +31,24 @@ select yn in "Yes" "No"; do
     esac
 done
 
-sudo apt-get update --fix-missing && sudo apt-get install -f && sudo apt-get install trash-cli && trash-empty 30 && sudo apt-get install localepurge && sudo apt-get install deborphan && deborphan --guess-all && sudo deborphan --guess-data | xargs sudo aptitude -y purge && sudo deborphan | xargs sudo apt-get -y remove --purge && sudo journalctl --vacuum-size=100M && sudo apt-get install preload && sudo apt-get upgrade -y && sudo flatpak update -y && sudo apt-get autoremove --purge -y && sudo apt-get clean && sudo apt-get autoclean && rm -rf ~/.cache/thumbnails/* && sudo flatpak uninstall --unused -y && sudo snap refresh && sudo snap set system refresh.retain=2 && xmessage "All done! Enjoy your Linux system! :D"
+sudo apt update --fix-missing
+sudo apt install -f
+sudo apt install trash-cli
+trash-empty 30
+sudo apt install localepurge
+sudo apt install deborphan
+deborphan --guess-all
+sudo deborphan --guess-data | xargs sudo aptitude -y purge
+sudo deborphan | xargs sudo apt-get -y remove --purge
+sudo journalctl --vacuum-size=100M
+sudo apt install preload
+sudo apt upgrade -y
+sudo flatpak update -y
+sudo apt autoremove --purge -y
+sudo apt clean
+sudo apt autoclean
+rm -rf ~/.cache/thumbnails/*
+sudo flatpak uninstall --unused -y
+sudo snap refresh
+sudo snap set system refresh.retain=2
+xmessage "All done! Enjoy your Linux system! :D"
